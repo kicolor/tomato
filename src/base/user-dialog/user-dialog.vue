976baed6c71cfea2f20d72a02590246cb1b98642
kicolor <mr_gold@yeah.net>
2018-08-20 15:37:49 +0800

    modify

--- src/base/user-dialog/user-dialog.vue
+++ src/base/user-dialog/user-dialog.vue
@@ -13,12 +13,13 @@
     <x-dialog v-model="loginDialog" hide-on-blur :dialog-style="{'max-width': '100%', width: '80%', height: 'auto', 'background-color': '#333'}">
       <div class="dialog-content vux-group">
         <p>{{header}}</p>
-        <x-input v-show="isEmail" is-type="email" title="邮箱" class="vux-dialog-input" :value="email" required :show-clear="false" ref="email"></x-input>
-        <x-input v-show="isAccount" type="text" title="账号" class="vux-dialog-input" :value="account" required ref="account"></x-input>
-        <x-input v-show="isPassword" type="password" title="密码" class="vux-dialog-input" :value="password" required ref="password"></x-input>
+        <x-input v-show="isAccount" type="text" :title="accountText" class="vux-dialog-input" :value="account" required ref="account"></x-input>
+        <x-input v-show="isEmail" is-type="email" :title="emailText" class="vux-dialog-input" :value="email" required :show-clear="false" ref="email"></x-input>
+        <x-input v-show="isPassword" type="password" :title="passwordText" class="vux-dialog-input" :value="password" required ref="password"></x-input>
+        <x-input v-show="isCheckPassword" type="password" :title="checkPasswordText" class="vux-dialog-input" :value="checkPassword" required ref="checkPassword"></x-input>
         <span @click="link" ref="left">{{linkLeft}}</span>
         <span @click="link">{{linkRight}}</span>
-        <button class="dialog-btn" @click="submit">确定</button>
+        <button class="dialog-btn" @click="submit">{{submitText}}</button>
       </div>
       <x-icon class="dialog-close" type="close" @click="close"></x-icon>
     </x-dialog>
@@ -37,15 +38,36 @@
         accountDialog: false,
         loginDialog: false,
         flag: 0,   // 0 登录、1 注册、2 重置密码
-        isEmail: true,
+        isEmail: false,
         isAccount: true,
         isPassword: true,
+        isCheckPassword: false,
         header: '登录',
         linkLeft: '忘记密码',
         linkRight: '注册',
+        accountText: '账号',
+        emailText: '关联邮箱',
+        passwordText: '密码',
+        checkPasswordText: '确认密码',
+        submitText: '确认',
         email: '',
         account: '',
-        password: ''
+        password: '',
+        checkPassword: ''
+      }
+    },
+    computed: {
+      emailValid() {
+        return this.$refs.email.valid
+      },
+      accountValid() {
+        return this.$refs.account.valid
+      },
+      passwordValid() {
+        return this.$refs.password.valid
+      },
+      checkPasswordValid() {
+        return this.$refs.checkPassword.valid
       }
     },
     methods: {
@@ -58,31 +80,35 @@
           this.isEmail = false
           this.isAccount = true
           this.isPassword = true
+          this.isCheckPassword = false
           this.header = '登录'
           this.linkLeft = '忘记密码'
           this.linkRight = '注册'
+          this.submitText = '确认'
           this.flag = 0
         } else if (e.target.innerText === '注册') {
           this.isEmail = true
           this.isAccount = true
           this.isPassword = true
+          this.isCheckPassword = true
           this.header = '注册'
           this.linkLeft = '忘记密码'
           this.linkRight = '登录'
+          this.submitText = '确认'
           this.flag = 1
         } else if (e.target.innerText === '忘记密码') {
           this.isEmail = true
           this.isAccount = true
           this.isPassword = false
-          this.header = '找回密码'
+          this.isCheckPassword = false
+          this.header = '重置密码'
           this.linkLeft = '登录'
           this.linkRight = '注册'
+          this.submitText = '发送邮件'
           this.flag = 2
         }
       },
       submit() {
-        console.log('isValid', this._isValid())
-        console.log('submit')
         if (this.flag === 0) {
           this._login()
         } else if (this.flag === 1) {
@@ -92,18 +118,34 @@
         }
       },
       _login() {
+        if (this.accountValid && this.passwordValid) {
 
+        } else {
+
+        }
       },
       _register() {
-        let user = {
-          email: this.email,
-          username: this.account,
-          password: this.password
+        if (this.emailValid && this.accountValid && this.passwordValid && this.checkPasswordValid) {
+          if (this.password === this.checkPassword) {
+            console.log('password', this.password)
+            console.log('checkPassword', this.checkPassword)
+            let user = {
+              email: this.email,
+              username: this.account,
+              password: this.password
+            }
+            addUser(user)
+          } else {
+            console.log('密码不一致')
+          }
+        } else {
+
         }
-        addUser(user)
       },
       _reset() {
+        if (this.accountValid && this.passwordValid) {
 
+        }
       },
       _isValid() {
         return this.$refs.email.valid && this.$refs.account.valid && this.$refs.password.valid
